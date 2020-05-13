b9ad52f65908a8f78d7c9ea37d34d3243ab77785
rmmahajan <manvi.mahajan20@gmail.com>
2020-05-13 19:48:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_KEY = "883e79e49fdf6a3421620c7268552355";
+
+class App extends Component {
+
+  state = {
+    location: ""
+  };
+
+  fetchData = (event) =>{
+
+    event.preventDefault();
+    console.log(this.state.location);
+
+  };
+
+  changeLocation = (event) =>{
+
+    this.setState({
+      location: event.target.value
+    });
+  };
+
+  render()
+  {
+
+    
+
+    return (
+      <div>
+        <h1>Weather App</h1>
+        <form onSubmit={this.fetchData}>
+          <label>To know weather , Please enter the info</label>
+          <input type="text" placeholder={"City, Country"} onChange={this.changeLocation} value={this.state.location}></input>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
